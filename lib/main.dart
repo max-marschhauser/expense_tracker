979b53b0aa17d0c9b023a740bce7bbd1af925bbe
Max Marschhauser <max.marschhauser@gmail.com>
2023-06-05 09:16:36 +0200

    adding data model

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/expenses.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Text('Dummy text neki novi kao'),
+      home: Expenses(),
     );
   }
 }
