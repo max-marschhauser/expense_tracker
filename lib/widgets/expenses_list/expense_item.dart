fad70a04fca9ddc10e2f353d6edc18fc4a3e69a0
Max Marschhauser <max.marschhauser@gmail.com>
2023-06-06 12:21:51 +0200

    adding icons and formatting date

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -8,6 +8,33 @@ class ExpenseItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('ff');
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text(
+                    '\$${expense.amount.toStringAsFixed(2)}'), // to string as fixed zaokružuje decimalna mjesta na određenoj dužini
+                const Spacer(), // u rowu će uzeti sav preostali prostor (nakon što text i row uzmu potreban prostor)
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 8),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
